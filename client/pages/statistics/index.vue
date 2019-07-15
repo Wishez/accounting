e0c7727144aa8081a87aaa15d49450ece64720ca
Filipp Zhuravlev <fzhuravlev@at-consulting.ru>
2019-07-15 18:39:07 +0300

    Save

---
+++ client/pages/statistics/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="container">
+    <h1>Statistics</h1>
+  </section>
+</template>
+
+<script>
+
+export default {
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
+
