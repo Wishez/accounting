75b2475ee2005610907e4258f8a20764dc85ecef
Filipp Zhuravlev <fzhuravlev@at-consulting.ru>
2019-07-30 20:21:35 +0300

    Make account form

---
+++ client/components/AccountForm.vue
@@ -0,0 +1,141 @@
+<template>
+  <form v-if="isLoggedIn" @submit.prevent="onSubmit">
+    <h1>Счёт</h1>
+    <slug-field
+      nameLabelText="Имя счёта"
+      namePlaceholderText="Ivan Bank"
+      slugPlaceholderText="ivan-bank"
+      :slugValue="this.account.slug"
+      :nameValue="this.account.name"
+      @slugInput="value => {
+        slug = value
+        payload.slug = value
+      }"
+      @nameInput="value => {
+        name = value
+        payload.name = value
+      }"
+    />
+
+    <p v-if="isError" class="error">{{errorMessage}}</p>
+
+    <base-button type="submit">
+      Сохранить
+    </base-button>
+  </form>
+</template>
+
+<script>
+import { createAccountGql, updateAccountGql } from '~/constants/gql'
+import { popupsNames } from '~/constants/popups'
+
+export default {
+  name: 'AccountForm',
+  computed: {
+    account() {
+      return this.$lodash.get(this.$store.state.popups.payload, popupsNames.ACCOUNT, {})
+    },
+    isLoggedIn() {
+      return this.$store.state.auth.isLoggedIn
+    },
+  },
+  data() {
+    return {
+      isError: false,
+      errorMessage: '',
+      name: '',
+      slug: '',
+      payload: {
+        name: '',
+        slug: '',
+      },
+    }
+  },
+
+  mounted() {
+    const { slug, name } = this.account 
+    if (name) {
+      this.payload.name = name
+      this.payload.slug = slug
+      this.slug = slug
+      this.name = name
+    }
+  },
+
+  methods: {
+
+    onSubmit() {
+      this.clearErorState()
+      if (this.account.slug) this.updateAccount()
+      else this.createAccount()
+    },
+
+    createAccount() {
+      this.makeMutationRequest(createAccountGql, 'createAccount')
+    },
+
+    closePopup() {
+      this.$store.commit('popups/closePopups')
+    },
+
+    handleAccountResponse({ data: responseData }, requestName) {
+      const { isSuccess, data } = responseData[requestName]
+      return isSuccess ? data : this.showErrorAccountRequest()
+    },
+
+    clearErorState() {
+      this.errorMessage = ''
+      this.isError = false
+    },
+
+    updateAccount() {
+      this.makeMutationRequest(updateAccountGql, 'updateAccount', { uuid: this.account.id })
+    },
+
+    async makeMutationRequest(mutation, requestName, payload) {
+      try {
+        const response = await this.$apollo.mutate({
+          mutation,
+          variables: {
+            ...payload,
+            payload: this.payload
+          }
+        }).then(response => this.handleAccountResponse(response, requestName))
+          .catch(this.showErrorAccountRequest)
+
+        if (response.id) this.closePopup()
+      } catch (e) {
+        console.error(e)
+        this.showErrorAccountRequest()
+      }
+    },
+
+    handleError(message) {
+      this.isError = true
+      this.errorMessage = message
+    },
+    
+    showErrorAccountRequest() {
+      this.handleError('Не удалось создать или обновить счёт')
+    },
+
+
+  },
+  // watch: {
+  //   name(value) {
+  //     this.payload.name = value
+  //     const formattedSlug = this.$lodash.transliteText(value, '-')
+  //     this.slug = formattedSlug
+  //     this.payload.slug = formattedSlug
+  //     return value
+  //   },
+
+  //   slug(value) {
+  //     const formattedSlug = this.$lodash.transliteText(value, '-')
+  //     this.payload.slug = formattedSlug
+  //     this.slug = formattedSlug
+  //     return slug
+  //   }
+  // },
+}
+</script>
