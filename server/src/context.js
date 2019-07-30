ca58d3086cccfda3d140dabcb1ddd0891c502f16
Filipp Zhuravlev <shiningfinger@list.ru>
2019-07-30 20:29:46 +0300

    Configure user context

--- server/src/context.js
+++ server/src/context.js
@@ -2,12 +2,13 @@ const get = require('lodash/get')
 const verifyToken = require('./datasources/auth').verifyToken
 
 module.exports = async ({ req }) => {
-    const accessToken = get(req.cookies, 'access_token') || get(req.headers, 'authorization', '').split(' ')[1]
+    const accessToken = get(req.headers, 'authorization', '').split(' ')[1]
     const validateTokenResponse = accessToken ? await verifyToken(accessToken) : {}
     const { email, detail } = validateTokenResponse
     const isAuth = Boolean(email)
     return { 
         message: isAuth ? null : detail,
+        token: accessToken,
         isAuth,
         email,
     }
