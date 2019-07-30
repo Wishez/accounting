05aa3b210f75867b8fad1bc3ff1379dc9b281873
Filipp Zhuravlev <shiningfinger@list.ru>
2019-07-30 20:27:37 +0300

    Refactor server api

--- server/src/datasources/auth.js
+++ server/src/datasources/auth.js
@@ -1,14 +1,14 @@
 const { RESTDataSource } = require('apollo-datasource-rest')
 const jwt = require('jsonwebtoken')
 const get = require('lodash/get')
-const config = require('../env')
+const env = require('../env')
 const axios = require('axios')
 
 const getParsedToken = (token) => jwt.decode(token, {complete: true})
 class AuthAPI extends RESTDataSource {
     constructor() {
         super()
-        this.baseURL = config.AUTH_API_URL
+        this.baseURL = env.AUTH_API_URL
     }
 
     async auth(email, password) {
@@ -18,14 +18,14 @@ class AuthAPI extends RESTDataSource {
     }
 
     async refresh(refreshToken) {
-        const result = await this.post('refresh', { refresh: refreshToken })
+        const result = await this.post('refresh/', { refresh: refreshToken })
         const { access, detail } = result
         const email = get(getParsedToken(access), 'payload.email')
         return { access, detail, email }
     }
 
     async verify(token) {
-        const result = await this.post('verify', { token })
+        const result = await this.post('verify/', { token })
         const email = get(getParsedToken(token), 'payload.email')
         const { detail } = result
         return { detail, email }
@@ -33,7 +33,7 @@ class AuthAPI extends RESTDataSource {
 }
 
 
-const verifyToken = async (token) => await axios.post(config.AUTH_API_URL + 'verify/', { token })
+const verifyToken = async (token) => await axios.post(env.AUTH_API_URL + 'verify/', { token })
     .then((response) => ({
         detail: response.data.detail,
         email: get(getParsedToken(token), 'payload.email'),
@@ -41,6 +41,6 @@ const verifyToken = async (token) => await axios.post(config.AUTH_API_URL + 'ver
 
 
 module.exports = {
-    api: new AuthAPI(),
+    api: AuthAPI,
     verifyToken,
 }
