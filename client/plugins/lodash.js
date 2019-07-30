a90be6e5ffa372be28f2774dd3b6f3f95344a5af
Filipp Zhuravlev <shiningfinger@list.ru>
2019-07-30 20:16:27 +0300

    Set plugins.

---
+++ client/plugins/lodash.js
@@ -0,0 +1,124 @@
+import Vue from 'vue'
+import get from 'lodash/get'
+import unionBy from 'lodash/unionBy'
+import uniqueId from 'lodash/uniqueId'
+import omit from 'lodash/omit'
+import isEmpty from 'lodash/isEmpty'
+import pick from 'lodash/pick'
+import last from 'lodash/last'
+import mapValues from 'lodash/mapValues'
+import cyrillicToTranslit from 'cyrillic-to-translit-js'
+import { moneyConfig } from '~/constants/config'
+
+const getHexFromHsl = (h, s, l) => {
+  h /= 360;
+  s /= 100;
+  l /= 100;
+  let r, g, b;
+  if (s === 0) {
+    r = g = b = l; // achromatic
+  } else {
+    const hue2rgb = (p, q, t) => {
+      if (t < 0) t += 1;
+      if (t > 1) t -= 1;
+      if (t < 1 / 6) return p + (q - p) * 6 * t;
+      if (t < 1 / 2) return q;
+      if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+      return p;
+    };
+    const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    const p = 2 * l - q;
+    r = hue2rgb(p, q, h + 1 / 3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1 / 3);
+  }
+  const toHex = x => {
+    const hex = Math.round(x * 255).toString(16);
+    return hex.length === 1 ? '0' + hex : hex;
+  };
+  return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
+}
+
+const getHslFromHex = (hex) => {
+  if (!hex) return {}
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
+  let r = parseInt(result[1], 16)
+  let g = parseInt(result[2], 16)
+  let b = parseInt(result[3], 16)
+  r /= 255, g /= 255, b /= 255
+
+  const max = Math.max(r, g, b), min = Math.min(r, g, b);
+  let hue, saturation, luminosity = (max + min) / 2;
+  if (max == min) {
+    hue = saturation = 0; // achromatic
+  } else {
+    const differance = max - min;
+    saturation = luminosity > 0.5 ? differance / (2 - max - min) : differance / (max + min);
+    switch (max) {
+      case r: hue = (g - b) / differance + (g < b ? 6 : 0); break;
+      case g: hue = (b - r) / differance + 2; break;
+      case b: hue = (r - g) / differance + 4; break;
+    }
+    hue /= 6;
+  }
+
+  return {
+    hue: hue * 255,
+    saturation: saturation * 100,
+    luminosity: luminosity * 100 ,
+  }
+}
+
+function changeColorLightness(col, amt) {
+  let usePound = false;
+
+  if (col[0] == "#") {
+      col = col.slice(1);
+      usePound = true;
+  }
+
+  let num = parseInt(col,16);
+
+  let r = (num >> 16) + amt;
+
+  if (r > 255) r = 255;
+  else if  (r < 0) r = 0;
+
+  let b = ((num >> 8) & 0x00FF) + amt;
+
+  if (b > 255) b = 255;
+  else if  (b < 0) b = 0;
+
+  let g = (num & 0x0000FF) + amt;
+
+  if (g > 255) g = 255;
+  else if (g < 0) g = 0;
+
+  return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
+}
+
+const getNumberFromMoney = value => {
+  const withoutSuffix = String(value).replace(moneyConfig.suffix, '')
+  const intValue = withoutSuffix.slice(0, -3).replace(/\./g, '')
+  const sum = `${intValue}.${withoutSuffix.slice(-2)}`
+  return Number(sum)
+}
+
+const translitCompiler = cyrillicToTranslit()
+const transliteText = (value, delimiters) => translitCompiler.transform(value.toLowerCase(), delimiters) 
+
+Vue.prototype.$lodash = {
+  get,
+  getHexFromHsl,
+  getHslFromHex,
+  unionBy,
+  changeColorLightness,
+  transliteText,
+  omit,
+  uniqueId,
+  isEmpty,
+  getNumberFromMoney,
+  pick,
+  last,
+  mapValues,
+}
---
+++ client/plugins/plugins.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import { Observable } from 'rxjs/Observable'
+import VueRx from 'vue-rx'
+
+Vue.use(VueRx, {
+  Observable,
+})
