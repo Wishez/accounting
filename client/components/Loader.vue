fda3d28a35bf798b8f73a04b228028793868ca6b
Filipp Zhuravlev <fzhuravlev@at-consulting.ru>
2019-07-30 20:20:50 +0300

    Make loader

---
+++ client/components/Loader.vue
@@ -0,0 +1,68 @@
+<template>
+  <div :class="{ 'logo-holder': true, logo_loader: shouldRotateLogo }">
+    <img 
+      :class="{
+        logo: true,
+        rotate: shouldRotateLogo,
+      }"
+      :src="appLogo"
+      alt="Logo"
+    />
+  </div>
+</template>
+
+<script>
+import appLogo from '~/assets/logo.svg'
+export default {
+  name: "Loader",
+  data: () => ({
+    appLogo
+  }),
+  props: {
+    shouldRotateLogo: {
+      type: Boolean,
+      default: true,
+    }
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~/assets/styles/config/_colors.sass';
+  @keyframes rotate {
+    to {
+      transform: rotate(360deg)
+    }
+  }
+
+  .rotate {
+    will-change: transform;
+    transform: rotate(0deg);
+    transform-origin: center;
+    animation: rotate infinite linear .6s;
+  }
+
+  .logo-holder {
+    display: inherit;
+  }
+
+  .logo {
+    width: 45px;
+    position: relative;
+    top: -5px;
+
+    &_loader {
+      width: calc(100% + 1em);
+      height: calc(100% + 1em);
+      position: absolute;
+      top: -.5em;
+      left: -.5em;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background: rgba(#fff, .7);
+      z-index: 100;
+    }
+  }
+</style>
