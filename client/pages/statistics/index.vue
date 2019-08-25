c0ed76d5f89694cd5cffeaf3dbe3ff04c19b6967
Filipp Zhuravlev <shiningfinger@list.ru>
2019-08-25 19:06:15 +0300

    Make placeholder at statistics page.

--- client/pages/statistics/index.vue
+++ client/pages/statistics/index.vue
@@ -1,19 +1,32 @@
 <template>
   <section class="container">
-    <h1>Статистика</h1>
+    <h1 class="placeholder__title">Статистика</h1>
+    <div class="placeholder">
+      <p class="placeholder__text">Здесь можно бует выгрузить статистику транзакций определённого счёта, определённого типа транзакции, за определённый период.</p>
+      <img :src="placeholder" alt="placeholder" class="placeholder__image">
+    </div>
   </section>
 </template>
 
 <script>
-
+import placeholder from '~/assets/cat.jpeg'
 export default {
-  components: {
-    
-  }
+  name: "StatisticsPage",
+
+  data: () => ({
+    placeholder
+  })
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.placeholder {
+  max-width: 60ch;
+  margin: auto;
 
+  &__title {
+    text-align: center;
+  }
+}
 </style>
 
