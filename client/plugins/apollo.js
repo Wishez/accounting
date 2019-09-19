41bc99a2a12536a13df961c5b5d6a2ff55c54564
Filipp Zhuravlev <shiningfinger@list.ru>
2019-09-19 11:17:32 +0300

    Remove ws endpoint

--- client/plugins/apollo.js
+++ client/plugins/apollo.js
@@ -1,10 +1,8 @@
 export default function() {
   let httpEndpoint = process.env.APOLLO_SERVER_HTTP || 'http://localhost:4002/api';
-  let wsEndpoint = process.env.APOLLO_SERVER_WS || 'ws://localhost:4002/api'
 
   if (process.client) {
     httpEndpoint = process.env.APOLLO_CLIENT_HTTP || 'http://localhost:4002/api'
-    wsEndpoint = process.env.APOLLO_CLIENT_WS || 'ws://localhost:4002/api'
   }
 
   return {
@@ -14,6 +12,5 @@ export default function() {
     },
     ssr: true,
     tokenName: 'access_token',
-    wsEndpoint,
   }
 }
