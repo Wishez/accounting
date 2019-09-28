3e58965a34091c917b5509241fb28517fbba5daa
Filipp Zhuravlev <shiningfinger@list.ru>
2019-09-28 17:35:17 +0300

    Add cookies domain.

--- client/config.js
+++ client/config.js
@@ -1,1 +1,2 @@
-export const graphqlApiAddress = 'http://localhost:8444/api'
+export const graphqlApiAddress = 'http://accounting.shining-present.ru:8444/api' //'http://localhost:8444/api'
+export const apolloCookieDomain = 'accounting.shining-present.ru'//'localhost'
